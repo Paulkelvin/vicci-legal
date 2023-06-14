01f07fa4bfbc9390ec556ef3eede5cec3dea13d8
Paulkelvin <Ibukunadesanya0@gmail.com>
2023-06-14 05:49:06 +0100

    change to server-side rendering and implement dynamic form in contact page

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navLinks = document.querySelector(".nav-links");
-let visibleSection = null;
 
 hamburgerMenu.addEventListener("click", () => {
   navLinks.classList.toggle("show");
@@ -10,30 +9,12 @@ hamburgerMenu.addEventListener("click", () => {
 
 document.querySelectorAll(".nav-links a").forEach((link) => {
   link.addEventListener("click", function (e) {
-    e.preventDefault();
+    // e.preventDefault();
 
     // Hide the nav links
     navLinks.classList.remove("show");
 
-    // Only perform these actions for links with a corresponding section
     if (this.getAttribute("href") !== "#") {
-      // Hide the current visible section
-      if (visibleSection) {
-        visibleSection.classList.remove("visible");
-        visibleSection.classList.add("hidden");
-        visibleSection.style.zIndex = 0;
-        // visibleSection.remove(); // Commented out to keep the clicked section in the DOM
-      }
-
-      // Show the clicked section
-      const section = document.querySelector(this.getAttribute("href"));
-      if (section) {
-        section.classList.remove("hidden");
-        section.classList.add("visible");
-        section.style.zIndex = 100;
-        visibleSection = section; // Update the visible section reference
-      }
-
       // Remove active class from all links
       document.querySelectorAll(".nav-links li").forEach((li) => {
         li.classList.remove("active");
@@ -41,13 +22,53 @@ document.querySelectorAll(".nav-links a").forEach((link) => {
 
       // Add active class to clicked link
       this.parentElement.classList.add("active");
-
-      // // Remove other sections from the DOM
-      // document.querySelectorAll("section").forEach((section) => {
-      //   if (section !== visibleSection) {
-      //     section.remove();
-      //   }
-      // });
     }
   });
+});
+
+const dropdownToggle = document.querySelector(".custom-dropdown-toggle");
+const dropdownMenu = document.querySelector(".custom-dropdown-menu");
+const dropdownItems = document.querySelectorAll(".custom-dropdown-menu li");
+const contactForm = document.getElementById("contact-form");
+const legalForm = document.getElementById("legalForm");
+const propertyForm = document.getElementById("propertyForm");
+const othersForm = document.getElementById("othersForm");
+
+// Toggle the dropdown menu
+dropdownToggle.addEventListener("click", function () {
+  dropdownMenu.classList.toggle("show");
+});
+
+// Handle dropdown item selection
+dropdownItems.forEach(function (item) {
+  item.addEventListener("click", function () {
+    const formType = this.getAttribute("data-form-type");
+
+    // Hide all form sections
+    legalForm.style.display = "none";
+    propertyForm.style.display = "none";
+    othersForm.style.display = "none";
+
+    // Show the selected form section
+    if (formType === "legal") {
+      legalForm.style.display = "block";
+    } else if (formType === "property") {
+      propertyForm.style.display = "block";
+    } else if (formType === "others") {
+      othersForm.style.display = "block";
+    }
+
+    // Update the dropdown toggle text
+    dropdownToggle.textContent = this.textContent;
+
+    // Close the dropdown menu
+    dropdownMenu.classList.remove("show");
+  });
+});
+
+// Close the dropdown menu when clicking outside
+window.addEventListener("click", function (event) {
+  if (!event.target.matches(".custom-dropdown-toggle")) {
+    dropdownMenu.classList.remove("show");
+  }
 });
